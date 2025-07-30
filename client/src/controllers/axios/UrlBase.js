bd607136a2d10ad96d46a277be07fc75bb7c815a
NicoolDqek <136205493+NicoolDqek@users.noreply.github.com>
2025-07-29 20:33:39 -0500

    arrelo ruta

--- client/src/controllers/axios/UrlBase.js
+++ client/src/controllers/axios/UrlBase.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const UrlBase =axios.create({
-baseURL:'https://k-api-phi.vercel.app/',
+baseURL:'https://k-api-phi.vercel.app',
 headers:{
  'Content-Type': 'application/json',
 }
